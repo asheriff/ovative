525c2b631cbe6b2b8c019c1e3111e755e3b1a861
Aaron Sheriff <aaron.sheriff@gmail.com>
2011-04-26 22:11:07 -0500

    Use images for slideshow border-radius

--- app.rb
+++ app.rb
@@ -10,5 +10,5 @@ end
 Ramaze.options.roots = [__DIR__]
 
 require __DIR__('model/init')
-require __DIR__('controller/routes')
 require __DIR__('controller/init')
+require __DIR__('controller/routes')
